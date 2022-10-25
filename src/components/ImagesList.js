c3724b5c7b18c6b04a259bf326f532d9e77bfe9f
nataliia-stepanenko <natalyss.contact@gmail.com>
2022-10-26 01:17:04 +0300

    Got over bug with strict mode and happily finilized hw23

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -3,30 +3,44 @@ import { useState, useEffect } from "react";
 const ImagesList = () => {
     const [page, setPage] = useState(1);
     const [photos, setPhotos] = useState([]);
+    const [isLoading, setLoading] = useState(true);
     
     useEffect(() => {
-        fetch('https://picsum.photos/v2/list?page={page}&limit=10')
+        setLoading(true);
+        fetch(`https://picsum.photos/v2/list?page=${page}&limit=10`)
             .then((response) => response.json())
             .then((result) => {
-                setPhotos(result)
-            })
+                setPhotos(prevState => prevState.concat(result));
+                setLoading(false);
+            });
     }, [page]);
 
     const handleShowMore = () => {
-        setPage(prevPage => prevPage + 1);
-    }
+        setPage((prevPage) => prevPage + 1);
+    };
+
+    if (isLoading) {
+        return (
+        <>
+            <div className="container">
+                <p className="loader">Loading...</p>
+            </div>
+        </>
+    )}
 
     return (
         <>
-            <h3>Image Gallery</h3>
-            <ul>
-                {photos.map(({id, download_url}) => (
-                    <li key={id}>
-                    <img src={download_url} alt={id} className="image"></img>
-                    </li>
-                ))}
-            </ul>
-            <button onClick={handleShowMore}>Show more</button>
+            <div className="container">
+                <h3>Image Gallery</h3>
+                <ul>
+                    {photos.map(({id, download_url}) => (
+                        <li key={id}>
+                        <img src={download_url} alt={id} className="image"></img>
+                        </li>
+                    ))}
+                </ul>
+                <button onClick={handleShowMore}>Show more</button>
+            </div>
         </>
     )
 }
